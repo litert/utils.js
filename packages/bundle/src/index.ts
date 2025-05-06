19f5c34c644e1a7e8fe931c918c10e19a34dd8f1
Angus ZENG <i.am.x.fenying@gmail.com>
2025-05-06 23:45:13 +0800

    build(pkg:network): added network utils package to bundle

--- packages/bundle/src/index.ts
+++ packages/bundle/src/index.ts
@@ -1,4 +1,5 @@
 export * as Array from '@litert/utils-array';
 export * as Object from '@litert/utils-object';
 export * as String from '@litert/utils-string';
+export * as Network from '@litert/utils-network';
 export * from '@litert/utils-ts-types';
